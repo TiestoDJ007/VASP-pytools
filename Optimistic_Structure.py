dbee89538a058832da552c6aed27a02ad33ec508
TiestoDJ007 <jackxiao007@outlook.com>
2020-03-23 10:16:05 +0800

    working

--- Optimistic_Structure.py
+++ Optimistic_Structure.py
@@ -1,22 +1,32 @@
 #!/home/universe/local_env/anaconda3/envs/abinit/bin/python
 from copy import deepcopy
 
+import numpy as np
 from pymatgen import Lattice
 from pymatgen.io.vasp import Poscar
 
 interface_position = 8.0
-interface_distance = 1.9557073022475353
+alpha_limit = 3.5
+beta_limit = 13.0
+interface_distance = 1.9591612063956567
 Module_Number = int(3)
-Situation_Number = int(1)
+Situation_Number = int(2)
 
 original_poscar = Poscar.from_file("Initial_Structure/POSCAR_M{0}_S{1}".format(Module_Number, Situation_Number))
 alpha_Ti_list = []
 beta_Ti_list = []
+alpha_Ti_sd = []
+beta_Ti_sd = []
 for number_atom in range(0, original_poscar.structure.num_sites):
     if original_poscar.structure[number_atom].z > interface_position:
         beta_Ti_list.append(number_atom)
     else:
         alpha_Ti_list.append(number_atom)
+
+    if original_poscar.structure[number_atom].z < alpha_limit:
+        alpha_Ti_sd.append(number_atom)
+    if original_poscar.structure[number_atom].z > beta_limit:
+        beta_Ti_sd.append(number_atom)
 
 alpha_Ti_max = 0
 for number_alpha in alpha_Ti_list:
@@ -27,6 +37,13 @@ beta_Ti_min = 100
 for number_beta in beta_Ti_list:
     if original_poscar.structure[number_beta].z < beta_Ti_min:
         beta_Ti_min = original_poscar.structure[number_beta].z
+
+sd_array = np.ones((original_poscar.natoms[0],3))
+for number_sd_alpha in alpha_Ti_sd:
+    sd_array[number_sd_alpha] = np.zeros(3)
+for number_sd_beta in beta_Ti_sd:
+    sd_array[number_sd_beta] = np.zeros(3)
+
 
 gap_alpha_beta = beta_Ti_min - alpha_Ti_max
 
@@ -51,6 +68,7 @@ for number_beta_Ti in beta_Ti_list:
                                                             number_beta_Ti].z - gap_alpha_beta + interface_distance
 
 total_structure.comment = "Optimistic_Structure"
+total_structure.selective_dynamics = sd_array
 
 total_structure.write_file(
     "Calculation_Files/Optimistic_Structure/POSCAR_SO_M{0}_S{1}".format(Module_Number, Situation_Number))
