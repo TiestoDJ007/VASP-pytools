ec6457a011da5a301bc9680507439b1f2b1d1b80
TiestoDJ007 <jackxiao007@outlook.com>
2020-04-19 09:24:19 +0800

    only move the impurity atom

--- CI-NEB_structure_gen.py
+++ CI-NEB_structure_gen.py
@@ -17,33 +17,40 @@ if __name__ == "__main__":
 
     Work_Dir = '/mnt/d/PycharmProjects/TI80/Calculation_Files/CI-NEB'
     Diffusion_structure = '/M3_S1'
-    Diffusion_element = '/Sn'
-    Transition_Paths = ['/41-01', '/41-07', '/57-07', '/57-19']
+    Diffusion_element = '/Al'
+    Transition_Paths = ['/41-01']
     image_value = 3
+
     init_Contcar_Paths = []
     final_Contcar_Paths = []
 
     for Transition_Path in Transition_Paths:
         init_Contcar_Paths.append(
-            '{}'.format(Work_Dir + Diffusion_structure + Diffusion_element + Transition_Path + '/ini/CONTCAR'))
+            '{}'.format(Work_Dir + Diffusion_structure + Diffusion_element + Transition_Path + '/POSCAR_Ini'))
         final_Contcar_Paths.append(
-            '{}'.format(Work_Dir + Diffusion_structure + Diffusion_element + Transition_Path + '/fin/CONTCAR'))
+            '{}'.format(Work_Dir + Diffusion_structure + Diffusion_element + Transition_Path + '/POSCAR_Fin'))
     for number_path in range(len(init_Contcar_Paths)):
         init_Poscar = Poscar.from_file(init_Contcar_Paths[number_path], False)
         final_Poscar = Poscar.from_file(final_Contcar_Paths[number_path], False)
+
+        sd = np.zeros((np.sum(init_Poscar.natoms), 3))
+        sd[-1] = [1, 1, 1]
+
+        init_Poscar.selective_dynamics = sd
+        final_Poscar.selective_dynamics = sd
 
         init_structure = init_Poscar.structure
         final_structure = final_Poscar.structure
 
         tst_obj = IDPPSolver.from_endpoints(endpoints=[init_structure, final_structure], nimages=int(image_value),
-                                            sort_tol=6.0)
-        neb_point = tst_obj.run(maxiter=1000, tol=1e-05, gtol=1e-3, step_size=0.1, max_disp=5.0, spring_const=5.0)
+                                            sort_tol=10.0)
+        neb_point = tst_obj.run(maxiter=10000, tol=1e-05, gtol=1e-3, step_size=0.1, max_disp=8.0, spring_const=5.0)
 
-        sd = D2_list_to_nparray(init_Poscar.selective_dynamics)
         for neb_number in range(len(neb_point)):
             image_number = '/' + '{:0>2d}'.format(neb_number)
-            image_path = Work_Dir + Diffusion_structure + Diffusion_element + Transition_Paths[
+            image_path = Work_Dir + Diffusion_structure + '_unre' + Diffusion_element + Transition_Paths[
                 number_path] + image_number
             os.makedirs(image_path)
             POSCAR_path = image_path + '/POSCAR'
             neb_point[neb_number].to(fmt="poscar", filename=POSCAR_path)
+        print('{} Done!'.format(Transition_Paths[number_path]))
--- Ci-Neb_structure_file.py
+++ Ci-Neb_structure_file.py
@@ -7,9 +7,9 @@ from pymatgen.io.vasp import Poscar
 
 Module_Number = int(3)
 Situation_Number = int(1)
-First_Atom = [41,57]
-Second_Atom = [1,2,7,8,19]
-Diffusion_Atom = 'Nb'
+First_Atom = [41, 57]
+Second_Atom = [1, 2, 7, 8, 19]
+Diffusion_Atom = 'Al'
 Original_POSCAR = Poscar.from_file(
     "Calculation_Files/Optimistic_Structure/POSCAR_SO_M{0}_S{1}".format(Module_Number, Situation_Number))
 
@@ -26,13 +26,16 @@ for atom_first in First_Atom:
         Ini_Structure.structure.remove_sites([atom_first - 1, atom_second - 1])
         Fin_Structure.structure.remove_sites([atom_first - 1, atom_second - 1])
 
-        sd_bool = Ini_Structure.selective_dynamics
-        sd_int = []
-        for bool_list in sd_bool:
-            int_list = np.array(bool_list).astype(int)
-            sd_int.append(int_list)
-        sd_int.append(np.array((1, 1, 1)))
-        sd_int = np.array(sd_int)
+        # sd_bool = Ini_Structure.selective_dynamics
+        # sd_int = []
+        # for bool_list in sd_bool:
+        #    int_list = np.array(bool_list).astype(int)
+        #    sd_int.append(int_list)
+        # sd_int.append(np.array((1, 1, 1)))
+        # sd_int = np.array(sd_int)
+
+        sd_int = np.zeros((np.sum(Ini_Structure.natoms), 3))
+        sd_int = np.row_stack((sd_int, [1, 1, 1]))
 
         Ini_Structure.structure.append(Diffusion_Atom, (Ini_x, Ini_y, Ini_z), coords_are_cartesian=True)
         Ini_Structure.selective_dynamics = sd_int
@@ -40,12 +43,12 @@ for atom_first in First_Atom:
         Fin_Structure.structure.append(Diffusion_Atom, (Fin_x, Fin_y, Fin_z), coords_are_cartesian=True)
         Fin_Structure.selective_dynamics = sd_int
 
-        structure_path = "Calculation_Files/CI-NEB/M{0}_S{1}/{2}".format(Module_Number, Situation_Number,
-                                                                         Fin_Structure.site_symbols[1])
+        structure_path = "Calculation_Files/CI-NEB/M{0}_S{1}/{2}_single".format(Module_Number, Situation_Number,
+                                                                                Fin_Structure.site_symbols[1])
         cineb_path = structure_path + "/{0:0>2d}-{1:0>2d}".format(atom_first, atom_second)
         os.makedirs(cineb_path)
         Ini_Structure.write_file(cineb_path + "/POSCAR_Ini")
         Fin_Structure.write_file(cineb_path + "/POSCAR_Fin")
 
-        #pot_file = Potcar(symbols=Fin_Structure.site_symbols, functional='PBE')
-        #pot_file.write_file(structure_path + "/POTCAR")
+        # pot_file = Potcar(symbols=Fin_Structure.site_symbols, functional='PBE')
+        # pot_file.write_file(structure_path + "/POTCAR")
