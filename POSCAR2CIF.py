038af7b182b682cce50e5728fddffeda8e6cc13c
TiestoDJ007 <jackxiao007@outlook.com>
2023-09-07 16:48:14 +0800

    uber generation fix

--- POSCAR2CIF.py
+++ POSCAR2CIF.py
@@ -1,8 +1,8 @@
 from pymatgen.io.cif import CifWriter
 from pymatgen.io.vasp import Poscar
 
-poscar_data = Poscar.from_file("Data_file/POSCAR")
+poscar_data = Poscar.from_file("Data_file/CONTCAR")
 structure = poscar_data.structure
 cif_writer = CifWriter(struct=structure)
 cif_writer.write_file("Data_file/layer_opt.cif")
-poscar_data.write_file("POSCAR_lattice_opt")
+#poscar_data.write_file("POSCAR_lattice_opt")
--- UBER_file_generator.py
+++ UBER_file_generator.py
@@ -2,75 +2,81 @@
 import os
 from copy import deepcopy
 
-from pymatgen import Lattice
+from pymatgen.core import Lattice
 from pymatgen.io.vasp import Poscar
 
-Module_Number = int(6)
-Situation_Number = int(1)
+#输入文件名称路径
+file_path_name="Data_file/POSCAR"
+#输入原始界面中间值
 interface_position = 10
-gap_distance = 0.01
-max_distance = 0.8923503859094439
+#界面步长
+interface_step = 0.01
+#最大界面间距
+max_interface_distance = 2
 initial_c = 0.8913503859094439
 up_c = 0.0
 
-original_poscar = Poscar.from_file(
-    "/mnt/c/Users/jackx/OneDrive/Calculation_Data/TC17_TI80/Optimistic_Structures/POSCAR_SO_M{}_S{}".format(Module_Number,
-                                                                                                         Situation_Number))
-alpha_Ti_list = []
-beta_Ti_list = []
+original_poscar = Poscar.from_file(file_path_name)
+#设置界面层A
+substance_top = []
+#设置界面层B
+substance_bottem = []
+#将界面层A和B的编号导入list中
 for number_atom in range(0, original_poscar.structure.num_sites):
     if original_poscar.structure[number_atom].z > interface_position:
-        beta_Ti_list.append(number_atom)
+        substance_bottem.append(number_atom)
     else:
-        alpha_Ti_list.append(number_atom)
+        substance_top.append(number_atom)
 
-alpha_Ti_max = 0
-for number_alpha in alpha_Ti_list:
-    if original_poscar.structure[number_alpha].z > alpha_Ti_max:
-        alpha_Ti_max = original_poscar.structure[number_alpha].z
+#查找界面距离最近的原子
+substance_top_cMax = int(0)
+for number_top in substance_top:
+    if original_poscar.structure[number_top].z > substance_top_cMax:
+        substance_top_cMax = original_poscar.structure[number_top].z
 
-beta_Ti_min = 100
-for number_beta in beta_Ti_list:
-    if original_poscar.structure[number_beta].z < beta_Ti_min:
-        beta_Ti_min = original_poscar.structure[number_beta].z
+substance_bottem_cMin = int(100)
+for number_bottem in substance_bottem:
+    if original_poscar.structure[number_bottem].z < substance_bottem_cMin:
+        substance_bottem_cMin = original_poscar.structure[number_bottem].z
 
-gap_alpha_beta = beta_Ti_min - alpha_Ti_max
+#计算两界面之间间距
+interface_width = abs(substance_bottem_cMin - substance_top_cMax)
 
-for number_step in range(1, int((max_distance - initial_c) / gap_distance) + 1):
-
-    path_name = "/mnt/c/Users/jackx/OneDrive/Calculation_Data/TC17_TI80/UBER_Files/M{0}_S{1}_Uber_{2}A_{3}A_{4}A/{5}".format(
-        Module_Number, Situation_Number,
-        gap_distance + initial_c,
-        gap_distance,
-        max_distance, number_step)
+#建立不同间距的界面模型
+for number_step in range(1, int((max_interface_distance - initial_c) / interface_step) + 1):
+#间距设置
+    step_distance = number_step*interface_step
+#建立UBER原子模型存放路径
+    path_name = "Data_file/UBER_Files/POSCAR_Uber_{0}A".format(step_distance)
     os.makedirs(path_name)
-
+#生成模型
     total_structure = deepcopy(original_poscar)
     total_structure.structure.lattice = Lattice.from_parameters(a=total_structure.structure.lattice.a,
                                                                 b=total_structure.structure.lattice.b,
-                                                                c=total_structure.structure.lattice.c - (
-                                                                        gap_alpha_beta - number_step * gap_distance - initial_c) * 2,
+                                                                #确保真空层厚度一致
+                                                                c=total_structure.structure.lattice.c - abs(
+                                                                        interface_width - number_step * interface_step  - initial_c) * 2,
                                                                 alpha=total_structure.structure.lattice.alpha,
                                                                 beta=total_structure.structure.lattice.beta,
                                                                 gamma=total_structure.structure.lattice.gamma)
-    for number_alpha_Ti in alpha_Ti_list:
-        total_structure.structure.sites[number_alpha_Ti].x = original_poscar.structure.sites[number_alpha_Ti].x
-        total_structure.structure.sites[number_alpha_Ti].y = original_poscar.structure.sites[number_alpha_Ti].y
-        total_structure.structure.sites[number_alpha_Ti].z = original_poscar.structure.sites[number_alpha_Ti].z + up_c
-    for number_beta_Ti in beta_Ti_list:
-        total_structure.structure.sites[number_beta_Ti].x = original_poscar.structure.sites[number_beta_Ti].x
-        total_structure.structure.sites[number_beta_Ti].y = original_poscar.structure.sites[number_beta_Ti].y
-        total_structure.structure.sites[number_beta_Ti].z = original_poscar.structure.sites[
-                                                                number_beta_Ti].z - gap_alpha_beta + number_step * gap_distance + initial_c + up_c
+    for number_substance_top in substance_top:
+        total_structure.structure.sites[number_substance_top].x = original_poscar.structure.sites[number_substance_top].x
+        total_structure.structure.sites[number_substance_top].y = original_poscar.structure.sites[number_substance_top].y
+        total_structure.structure.sites[number_substance_top].z = original_poscar.structure.sites[number_substance_top].z + up_c
+    for number_substance_bottem in substance_bottem:
+        total_structure.structure.sites[number_substance_bottem].x = original_poscar.structure.sites[number_substance_bottem].x
+        total_structure.structure.sites[number_substance_bottem].y = original_poscar.structure.sites[number_substance_bottem].y
+        total_structure.structure.sites[number_substance_bottem].z = original_poscar.structure.sites[
+                                                                    number_substance_bottem].z - interface_width + number_step * interface_step + initial_c + up_c
 
-    alpha_structure = deepcopy(total_structure)
-    alpha_structure.structure.remove_sites(beta_Ti_list)
-    alpha_structure.comment = "alpha_Ti"
-    beta_structure = deepcopy(total_structure)
-    beta_structure.structure.remove_sites(alpha_Ti_list)
-    beta_structure.comment = "beta_Ti"
-    total_structure.comment = "total_Ti"
+    substance_top_structure = deepcopy(total_structure)
+    substance_top_structure.structure.remove_sites(substance_bottem)
+    substance_top_structure.comment = "substance_top"
+    substance_bottem_structure = deepcopy(total_structure)
+    substance_bottem_structure.structure.remove_sites(substance_top)
+    substance_bottem_structure.comment = "substance_bottem"
+    total_structure.comment = "total_structure"
 
-    alpha_structure.write_file("{0}/POSCAR_alpha.vasp".format(path_name))
-    beta_structure.write_file("{0}/POSCAR_beta.vasp".format(path_name))
+    substance_top_structure.write_file("{0}/POSCAR_top.vasp".format(path_name))
+    substance_bottem_structure.write_file("{0}/POSCAR_bottem.vasp".format(path_name))
     total_structure.write_file("{0}/POSCAR_total.vasp".format(path_name))
