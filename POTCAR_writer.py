60d1b4793a2f599236e36d49441a0c3ba195af5b
TiestoDJ007 <jackxiao007@outlook.com>
2023-11-05 11:29:23 +0800

    231105

--- POTCAR_writer.py
+++ POTCAR_writer.py
@@ -1,4 +1,5 @@
 from pymatgen.io.vasp import Potcar
-potcar_symbols = ["Cr","Fe","N"]
+
+potcar_symbols = ["Cr", "Fe", "N", "W"]
 potcar = Potcar(potcar_symbols)
-potcar.write_file("POTCAR-total")
+potcar.write_file("Data_file/POTCAR-total")
